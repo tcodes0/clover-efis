4f6ca8b9f106918e6a8e9eb942c3d67135436c97
Raphael Thomazella <thomazella9@gmail.com>
2019-11-16 02:42:38 -0300

    chore(misc): sync script

---
+++ scripts/sync.sh
@@ -0,0 +1,35 @@
+#! /usr/local/bin/bash
+
+bailout() {
+  echo "$@"
+  exit
+}
+
+set -x
+
+RSYNC="rsync -rtvvuc"
+z170_PATH="./z170/EFI/"
+z170_DEVICE="disk0s1"
+
+if ioreg -l | grep -q iMac17,1; then
+  FROM="$z170_PATH"
+  DEVICE="$z170_DEVICE"
+fi
+if [ ! "$FROM" ]; then
+  bailout "failed to detect motheboard folder to use"
+fi
+
+if ! diskutil info "$DEVICE" | grep -q -E "Mounted:[ ]+Yes"; then
+  bailout "device not mounted"
+fi
+MOUNTPOINT=$(diskutil info "$DEVICE" | sed -nE "/Mount\ Point/p" | sed -E "s/   Mount[ ]Point:[ ]+([^ ]+)/\1/g")
+if [ ! "$MOUNTPOINT" ]; then
+  bailout "mountpoint detection problem"
+fi
+if [ ! -d "${MOUNTPOINT}/EFI" ]; then
+  bailout "mountpoint has no EFI folder"
+fi
+
+eval "$RSYNC $z170_PATH ${MOUNTPOINT}/EFI"
+
+set +x
